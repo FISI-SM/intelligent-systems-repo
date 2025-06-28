fceb982a3cd123d7714fe36da46a839a763203e0
Herminio <herminiopaucar@gmail.com>
2025-06-28 00:34:48 -0500

    upload new example of jade in Multiplataforma

---
+++ Multi-Agent System/jade-multiplataforma/jade-remote-launcher/agentes/RemoteAgent.java
@@ -0,0 +1,21 @@
+
+package agentes;
+
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+
+public class RemoteAgent extends Agent {
+    protected void setup() {
+        System.out.println(getLocalName() + ": esperando mensajes...");
+        addBehaviour(new jade.core.behaviours.CyclicBehaviour() {
+            public void action() {
+                ACLMessage msg = receive();
+                if (msg != null) {
+                    System.out.println(getLocalName() + " recibió: " + msg.getContent());
+                } else {
+                    block();
+                }
+            }
+        });
+    }
+}
